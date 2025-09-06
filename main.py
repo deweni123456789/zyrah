33e8a926bd6eeb07f768fdd0c4def42a82c261fb
deweni123456789 <schoford91@gmail.com>
2025-09-06 12:21:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from pyrogram import Client
 from modules.tiktok import register_tiktok
-from modules.yt_downloader import register_youtube
+from modules.youtube import register_youtube
 
 API_ID = int(os.getenv("API_ID", "5047271"))
 API_HASH = os.getenv("API_HASH", "047d9ed308172e637d4265e1d9ef0c27")
@@ -16,10 +16,12 @@ app = Client(
 
 # Register modules
 register_tiktok(app)
+register_youtube(app)
 
 print("🚀 Social Media Downloader Bot started...")
 
 app.run()
+
 
 
 
