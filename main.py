3c4663e1dc817a02d658277221672d0578624596
deweni123456789 <schoford91@gmail.com>
2025-09-06 14:18:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import os
-from dotenv import load_dotenv
 from pyrogram import Client
 from modules.tiktok import register_tiktok
+from modules.instagram import register_instagram
 
-load_dotenv()  # Load .env file
-
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID", "5047271"))
+API_HASH = os.getenv("API_HASH", "047d9ed308172e637d4265e1d9ef0c27"))
+BOT_TOKEN = os.getenv("BOT_TOKEN", "7896090354:AAFhFhcbEoJreu1vUZN-kY673pJqV62eMoU")
 
 app = Client(
     "social_downloader_bot",
@@ -16,10 +14,10 @@ app = Client(
     bot_token=BOT_TOKEN
 )
 
+# Register modules
 register_tiktok(app)
-
+register_instagram(app)   # ✅ Instagram included
 
 print("🚀 Social Media Downloader Bot started...")
+
 app.run()
-
-
